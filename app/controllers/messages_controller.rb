c7f7a9cfbbbe33a41f360c63c118474a8b43698b
Bren Cavallo <bcavallo@gc.cuny.edu>
2015-01-05 14:09:51 -0500

    fixed messages_controller for email delivery, still not working

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -62,6 +62,11 @@ class MessagesController < ApplicationController
     end
   end
 
+  def send_message
+    UserMailer.message_email().deliver
+    redirect_to "/messages"
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_message
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   resources :users
   root 'messages#index'
+  get :send_message, to: 'messages#send_message', as: :send_message
 end
