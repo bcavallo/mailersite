7d4eab02f4b56ca8538ad7cf2abc3d76b05f704a
Bren Cavallo <bcavallo@gc.cuny.edu>
2015-02-11 23:47:15 -0500

    added password authenication for users! now to continue with chap 7 of hartl for sign up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212180309) do
+ActiveRecord::Schema.define(version: 20150212024921) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "message"
@@ -20,10 +20,13 @@ ActiveRecord::Schema.define(version: 20141212180309) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
